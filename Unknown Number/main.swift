64b3d4d579285ba0e4ae09cf468c5a54efa0bfe9
homeless <vlad.sarkulov@gmail.com>
2023-09-08 18:31:13 +0300

    the first option

--- Unknown Number/main.swift
+++ Unknown Number/main.swift
@@ -5,7 +5,25 @@
 //  Created by @hmlssgd on 08.09.2023.
 //
 
-import Foundation
 
-print("Hello, World!")
+//генерация случайного числа
+let randomNamber = UInt8.random(in: 1...250)
 
+print("Компьютер случайным образом загадал число. Вам требуется отгадать его")
+// в переменную будут записываться числа с консоли
+var myNamber: String?
+//цикл с постпроверкой условия
+
+repeat {
+    print("Введите ваш вариант и нажмите Enter")
+    // получение значения с клавиатуры пользователя
+    myNamber = readLine()
+    //сравнение введенного и сгенерированного чисел
+    if (UInt8(myNamber!) == randomNamber) {
+        print("Вы угадали")
+    } else if (UInt8(myNamber!)! < randomNamber) {
+        print("Ваш вариант меньше загаданного числа")
+    } else if (UInt8(myNamber!)! > randomNamber) {
+        print("Ваш вариант больше загаданного числа")
+    }
+} while randomNamber != UInt8(myNamber!)
